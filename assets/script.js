728fefd46027e7fb80fe7302c51ea95f9e76f29f
unknown <nukasine@gmail.com>
2021-05-09 18:36:41 -0600

    starting work on the javascript portion of this app. Should have enough variables assigned to cooresponding components in the app to do an ajax call and get current weather info.

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+let searchForm = document.getElementById("city-search");
+let searchButton = document.getElementById("search-button");
+// let currentWeather= document.querySelector("current-weather-info")
+let cityName= document.getElementById("place");
+let cityTemp= document.getElementById("temperature");
+let cityHumid= document.getElementById("humidity");
+let cityWind= document.getElementById("wind");
+let cityUV= document.getElementById("uv-index");
